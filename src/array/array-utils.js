76edb29960f897c3a385cc33b8bccbd145db9285
nandawtek <nandawtek@gmail.com>
2017-08-14 23:20:22 +0200

    Generate new suite for deleteIf function in array-utils

--- src/array/array-utils.js
+++ src/array/array-utils.js
@@ -1,4 +1,4 @@
-function clear(array = []){
+function clear(array = []) {
     try{
         array.length = 0;
         return array;
@@ -9,14 +9,31 @@ function clear(array = []){
     }
 }
 
-function first(array){
+function first(array) {
     if (!Array.isArray(array)) {
         throw new TypeError('argument is not an array');
     }
     return array[0] || null;
 }
 
+function deleteIf(array, condition = () => undefined) {
+    if (!Array.isArray(array)) {
+        throw new TypeError('argument is not an array');
+    }
+    if(array.length === 0) return array;
+
+    for(let i = 0 ; i < array.length ;){
+        if(condition(array[i])){
+            array.splice(i, 1)
+        }else {
+            i++;
+        }
+    }
+    return array;
+}
+
 export {
     clear,
-    first
+    first,
+    deleteIf
 };
--- test/array-utils.test.js
+++ test/array-utils.test.js
@@ -1,4 +1,4 @@
-import {clear, first} from '../src/array/array-utils.js';
+import {clear, first, deleteIf} from '../src/array/array-utils.js';
 
 describe('Clear function for array', () =>{
     test('clear an array', () => {
@@ -30,5 +30,31 @@ describe('First function for array', () =>{
         const number = 1;
 
         expect(()=>{first(number)}).toThrow('argument is not an array');
+    });
+});
+
+describe('deleteIf function for array', () =>{
+    test('should detete all elements that match in condition', () => {
+        const array = [1,3,5,7,9];
+        const isGreaterThan5 = (number) => number > 5;
+        const result = [1,3,5];
+
+        expect(deleteIf(array, isGreaterThan5)).toEqual(result);
+    });
+    test('should return an empty array if array is empty', () => {
+        const array = [];
+        const isGreaterThan5 = (number) => number > 5;
+
+        expect(deleteIf(array, isGreaterThan5)).toEqual([]);
+    });
+    test('should do nothing is the condition is not matched', () => {
+        const array = [1,2,3];
+
+        expect(deleteIf(array)).toEqual([1,2,3]);
+    });
+    test('throws an error if argument is not an array', () => {
+        const number = 1;
+
+        expect(()=>{deleteIf(number)}).toThrow('argument is not an array');
     });
 });
