a4e9a1560aa297cd53f69dc26d2ea64580c50e75
nandawtek <nandawtek@gmail.com>
2017-08-14 22:30:37 +0200

    Generate suite for include similar funcionality test

--- test/array-utils.test.js
+++ test/array-utils.test.js
@@ -1,14 +1,15 @@
 import {clear} from '../src/array/array-utils.js';
 
-test('clear an array', () => {
-    const array = [1,2,3];
-    const result = [];
+describe('Clear function for array', () =>{
+    test('clear an array', () => {
+        const array = [1,2,3];
+        const result = [];
 
-    expect(clear(array)).toEqual(result);
-});
+        expect(clear(array)).toEqual(result);
+    });
+    test('throws an error if argument is not an array', () => {
+        const number = 1;
 
-test('throws an error if argument is not an array', () => {
-    const number = 1;
-
-    expect(()=>{clear(number)}).toThrow('argument is not an array');
+        expect(()=>{clear(number)}).toThrow('argument is not an array');
+    });
 });
